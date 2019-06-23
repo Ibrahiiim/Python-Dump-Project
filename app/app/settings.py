9446bf1a577be756a15e81a70da7da2ba9cd31cd
Ibrahiiim <ibrahimabuaquel@gmail.com>
2019-06-23 23:25:17 +0300

    Delete Database files

--- app/app/settings.py
+++ app/app/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%!*a+9e_3#p-wfch9waim4exk=8_8=n!)$gq$mk8jt4=te$y*u'
+SECRET_KEY = 'n$d83y-#u64a1!po^%#$_qwvom7j$a!$9n)p8apln*0to2+kwm'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
